df1cf25599cd7d3b79a16dfc09c180210fa1ebcd
Irina Lipovaya <irina.i.lipovaya@gmail.com>
2020-10-17 18:42:52 +0200

    Used useState hook, created custom hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Papper" animal="Bird" breed="Cockatel" />
-      <Pet name="Barsik" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
