c07a0df3eba0594066d07b2ec24f88b40c80a3f8
Irina Lipovaya <irina.i.lipovaya@gmail.com>
2020-10-18 10:04:12 +0200

    Added reach router

---
+++ src/Details.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Details = (props) => {
+  return (
+    <div>
+      <h1>Hey, that is details page</h1>
+    </div>
+
+    // <pre>
+    //   <code>{ JSON.stringify(props, null, 4) }</code> {/* to debug props */}
+    // </pre>
+  );
+};
+
+export default Details;
